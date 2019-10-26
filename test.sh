ed9cfa8fe05f8219fef8c260037c8f8360c97e9f
Jiří Klepl <jiriklepl@seznam.cz>
2019-10-26 03:50:08 +0200

    improved the test.sh script

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if command -v colordiff > /dev/null; then
+    diff="colordiff"
+else
+    diff="diff"
+fi
+
 ls public-cpp/*.out | {
     while read name; do
         output="public-cpp/"`basename "$name"`
         expected="public-tests/"`basename "$name"`
         echo  "comparing $output (left) against $expected (right)"
-        diff -y "$output" "$expected"
+        $diff -y "$output" "$expected"
     done
-} | less
+} | less -R

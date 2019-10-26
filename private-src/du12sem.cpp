c8c962974cee91ca8e379f73a040c2bbdb225525
Jiří Klepl <jiriklepl@seznam.cz>
2019-10-26 19:52:00 +0200

    added string and number support

--- private-src/du12sem.cpp
+++ private-src/du12sem.cpp
@@ -17,10 +17,69 @@ namespace mlc {
 
     std::string& upper_case(std::string& from) {
         for (auto&& c : from) {
-            c |= 32;
+            if (c >= 'a' && c <= 'z') {
+                c ^= (1UL << 5);
+            }
         }
 
         return from;
+    }
+
+    std::string& un_apostrophe(std::string& from) {
+        for (std::size_t i = 0; i < from.size(); ++i) {
+            if (from[i] == '\'') {
+                from.erase(i, 1);
+            }
+        }
+
+        return from;
+    }
+
+    ls_int_type::value_type convert_int(
+        const char* from,
+        decltype(mlc::MlaskalCtx::curline) line
+    ) {
+        ls_int_type::value_type number = 0;
+        const char* where = from;
+
+        for (; *where != '\0'; ++where) {
+            number *= 10;
+            if (  // checking for both variants, we dont know the architecture
+                number < 0 ||
+                (std::uint_least32_t)number >= (std::uint_least32_t)(1U << 31)
+            ) {
+                message(DUERR_INTOUTRANGE, line, from);
+
+                ++where;
+                break;
+            }
+
+            number += (*where - '0');
+            if (  // checking for both variants, we dont know the architecture
+                number < 0 ||
+                (std::uint_least32_t)number >= (std::uint_least32_t)(1U << 31)
+            ) {
+                message(DUERR_INTOUTRANGE, line, from);
+
+                ++where;
+                break;
+            }
+        }
+
+        for (; *where != '\0'; ++where) {
+            number *= 10;
+            number += (*where - '0');
+        }
+
+        return number & ((1UL << 31) - 1UL);
+    }
+
+    ls_real_type::value_type convert_real(const std::string& from) {
+        std::istringstream from_stream{from};
+        ls_real_type::value_type number;
+
+        from_stream >> number;
+        return number;
     }
 
 }
--- private-src/du12sem.hpp
+++ private-src/du12sem.hpp
@@ -11,7 +11,11 @@
 #ifndef DU12SEM_HPP_
 #define DU12SEM_HPP_
 
+#include <cstddef>
+
 #include <string>
+#include <sstream>
+
 #include "literal_storage.hpp"
 #include "flat_icblock.hpp"
 #include "dutables.hpp"
@@ -21,7 +25,11 @@
 namespace mlc {
 
     std::string& upper_case(std::string&);
-
+    std::string& un_apostrophe(std::string&);
+    ls_real_type::value_type convert_real(const std::string&);
+    ls_int_type::value_type convert_int(
+        const char* from,
+        decltype(mlc::MlaskalCtx::curline) line);
 }
 
 #endif  // DU12SEM_HPP_

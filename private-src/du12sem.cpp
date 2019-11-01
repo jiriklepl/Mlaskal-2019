0f3aba6c3cd41941305970c295af394ff7388bd7
jiriklepl <jiriklepl@seznam.cz>
2019-11-01 20:55:42 +0100

    removed goto and undefined behaviour

--- private-src/du12sem.cpp
+++ private-src/du12sem.cpp
@@ -44,12 +44,14 @@ namespace mlc {
         const char* from,
         decltype(mlc::MlaskalCtx::curline) line
     ) {
-        ls_int_type::value_type number = 0;
-        const char* where = from;
         constexpr std::uint_least32_t limit = 1U << 31;
+        constexpr std::uint_least32_t mask = (1UL << 31) - 1UL;
+
+        std::uint_least32_t number = 0;
+        const char* where = from;
 
         for (; *where != '\0'; ++where) {
-            if (static_cast<std::uint_least32_t>(number) >= limit / 10) {
+            if (number >= limit / 10) {
                 message(DUERR_INTOUTRANGE, line, from);
                 break;
             }
@@ -58,9 +60,12 @@ namespace mlc {
 
             char digit = (*where - '0');
 
-            if (static_cast<std::uint_least32_t>(number) >= limit - digit) {
+            if (number >= limit - digit) {
                 message(DUERR_INTOUTRANGE, line, from);
-                goto for_rest_digit;
+
+                number += digit;
+                ++where;
+                break;
             }
 
             number += digit;
@@ -68,11 +73,10 @@ namespace mlc {
 
         for (; *where != '\0'; ++where) {
             number *= 10;
-        for_rest_digit:
             number += (*where - '0');
         }
 
-        return number & ((1UL << 31) - 1UL);
+        return static_cast<ls_int_type::value_type>(number & mask);
     }
 
     ls_real_type::value_type convert_real(const std::string& from) try {
